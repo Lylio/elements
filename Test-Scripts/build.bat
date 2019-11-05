5318b9bee83e21859287bc6682a71d300d09efa2
lylio <cxz@gmx.com>
2019-11-05 21:19:43 +0000

    MINOR : Updated test scripts

---
+++ Test-Scripts/build.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+echo This is the build.bat file...BAT!
+
+SET a=5
+SET b=9
+SET /a total=%a+%b
+
+echo The total in the Python file is: %total%
+
+echo Attempting to call adder.py script... (python adder.py)
+python adder.py
+
+echo Attempting to call adder.py script... @py adder.py)
+@py adder.py
+
