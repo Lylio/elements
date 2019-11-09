583cadb4521c282efb65eed4148fbfc5f6361a04
Lyle Christine <cxz@gmx.com>
2019-11-09 22:02:22 +0000

    update

--- Test-Scripts/test.sh
+++ Test-Scripts/test.sh
@@ -2,11 +2,11 @@
 
 echo "This is the test.sh file...."
 echo ""
-file="employees.txt"
+file="./Test/Scripts/employees.txt"
 echo "Print managers from the employees text file..."
 awk '/manager/ {print}' $file
 printf "\n"
 echo "-----------------------"
 echo ""
 echo "Print employee name and their salary from the employees text file..."
-awk '{print $1,$4}' employees.txt
+awk '{print $1,$4}' $file
